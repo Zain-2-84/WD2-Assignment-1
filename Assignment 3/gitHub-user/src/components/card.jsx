841752e110aab9fa55265a98477e21432e51dd8d
Zain-2-84 <zainazhar.qureshi.2.84@gmail.com>
2025-08-03 02:05:49 +0500

    Added Assignment 3.

---
+++ Assignment 3/gitHub-user/src/components/card.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./styles/card.css";
+
+function Card () {
+    return (
+        <main className="cardHolder">
+            <div className="card">
+                <div className="card-header"></div>
+                <div className="card-avatar">
+                    <img src="../public/images/default_github_profile_icon" alt="" srcset="" />
+                </div>
+                <div className="card-info">
+                    <div className="card-info__default">
+                        <section>
+                            <h3>Alhaan</h3>
+                            <p>Age: 26</p>
+                        </section>
+
+                        <span>
+                            <p>Location: Karachi</p>
+                        </span>
+                    </div>
+                    <div className="line-break"></div>
+                    <div className="card-info__expand">
+                        <div>
+                            <span>
+                                <p className="float-left">
+                                    Followers
+                                    </p>
+                                <p className="number">2K</p>
+                            </span>
+                        </div>
+                        <div>
+                            <span>
+                                <p className="float-left">
+                                    Likes
+                                    </p>
+                                <p className="number">10K</p>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </main>
+    );
+}
+
+export default Card;
