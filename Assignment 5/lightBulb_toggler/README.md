298a6d3f6d0b843821b3094fb7f9007f4910a7aa
Zain-2-84 <zainazhar.qureshi.2.84@gmail.com>
2025-08-09 01:36:42 +0500

    Added Assignment 5 'Light bulb toggler'.

---
+++ Assignment 5/lightBulb_toggler/README.md
@@ -0,0 +1,3 @@
+Why do we use onBulb instead of toggle function?
+
+Because toggle doesn't have a true or a false value, onBulb does. So if you use toggle, React can't decide if the value is true or is changing.
---
+++ Assignment 5/lightBulb_toggler/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+// import
+import './App.css'
+
+function App() {
+  const [onBulb, setOnBulb] = useState(false);
+
+  function toggle() {
+    setOnBulb(!onBulb);
+  }
+
+  return (
+    <>
+
+    <div className={ 'lightBulb-with-button'} style={ { backgroundColor: onBulb ? 'white' : 'black'} }>
+
+    <h1 className={"heading"} style={ {color: onBulb ? 'black' : 'white'} }>
+      Light bulb
+    </h1>
+      <div className='lightBulb-Holder'>
+        {onBulb === false ? (
+            <img src="/images/Light-bulb-off-Bg-removed.png" alt="Light bulb off" srcset=""/>
+          ) : (
+            <img src="/images/Light-bulb-on-Bg-removed.png" alt="Light bulb on" srcset="" />
+          )
+        }
+      </div>
+      
+        <div className='toggleButton-Holder'>
+          <div onClick={toggle}>{ onBulb === false ? <img src='../images/toggle-button/toggle-button-Off-Bg-removed.png' /> : <img src='../images/toggle-button/toggle-button-On-Bg-removed.png'/> }</div>
+        </div>
+    </div>
+    </>
+  );
+}
+
+export default App
