679757db3eb1213da5c68d01a3a9d5b6cad4c6fc
Zain-2-84 <zainazhar.qureshi.2.84@gmail.com>
2025-10-03 21:02:01 +0500

    Completed the 'Student-Feedback' & 'Movie-Searching Project'.

--- Assignment-10/movieSearch/src/App.jsx
+++ Assignment-10/movieSearch/src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from 'react'
 import MovieSearch from './components/MovieSearch'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <MovieSearch/>
--- Assignment-10/movieSearch/src/components/MovieSearch.jsx
+++ Assignment-10/movieSearch/src/components/MovieSearch.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
+import '../styles/MovieSearch.css'
 
 const MovieSearch = () => {
-  const [query, setQuery] = useState("spiderman");
+  const [query, setQuery] = useState("one percenter");
   const [movie, setMovie] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
@@ -12,7 +13,7 @@ const MovieSearch = () => {
       setError("");
       try {
         const res = await fetch(
-          `https://www.omdbapi.com/?t=${query}&apikey=your_api_key`
+          `https://www.omdbapi.com/?t=${query}&apikey=267ee006`
         );
         const data = await res.json();
         if (data.Response === "True") {
@@ -30,25 +31,35 @@ const MovieSearch = () => {
     if (query) {
       fetchMovie();
     }
-  }, [query]); // query change hote hi new search
+  }, [query]); // When the user searches anything new the query updates.
+
+  console.log({ movie });
 
   return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <h2>🎬 Movie Search App</h2>
-      <input
-        type="text"
-        placeholder="Enter movie name..."
-        onKeyDown={(e) => e.key === "Enter" && setQuery(e.target.value)}
-      />
-      {loading && <p>Loading...</p>}
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {movie && (
-        <div>
-          <h3>{movie.Title} ({movie.Year})</h3>
-          <img src={movie.Poster} alt={movie.Title} />
-          <p>⭐ Rating: {movie.imdbRating}</p>
-        </div>
-      )}
+    <div className="movie-search-page">
+      <div className="movie-search-everything" style={{ textAlign: "center" }}>
+        <h2 className="movie-search-page-title">🎬 Movie Search App</h2>
+        <input
+          type="text"
+          placeholder="Enter movie name..."
+          onKeyDown={(e) => e.key === "Enter" && setQuery(e.target.value)}
+        />
+        {loading && <p>Loading...</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        {movie && (
+          <div className="movie-details">
+            <div className="movie-imageAndTitle">
+              <img className="movie-poster" id="movie-poster-id" src={movie.Poster} alt={movie.Plot} />
+              <h3 className="movie-title">{movie.Title} ({movie.Year})</h3>
+            </div>
+
+            <p className="movie-rating">⭐ imdbRating: {movie.imdbRating}</p>
+            <p className="movie-genre">Genre: {movie.Genre}</p>
+            <p className="movie-runtime">Runtime: {movie.Runtime}</p>
+            {/* <p className="movie-plot">Plot: {movie.Plot}</p> */}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
